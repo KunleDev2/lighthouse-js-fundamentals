6e8e2add4d3beeef2672fee28f751a585e35b1e6
Adekunle Oshodi <kunleoshodi856@gmail.com>
2023-03-27 14:34:24 +0100

    function to calculate in number is odd or not

---
+++ odd.js
@@ -0,0 +1,12 @@
+// function to check if number is odd
+
+function isOdd(num) {
+  if(num % 3 === 0){
+    return true;
+  }else{
+    return false;
+  }
+}
+
+console.log("3 is odd: " + isOdd(3));
+console.log("8 is odd: " + isOdd(8));
